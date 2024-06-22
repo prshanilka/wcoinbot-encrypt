74710ffe9d7eb2af531c46c41b0aa3752774cbe2
prshanilka <ramesh.shanilka@gmail.com>
2024-06-22 19:51:49 +0530

    sdadas

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.post('/encrypt', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
